003d43e55e9942355b41568dbc3fa9e8c81bd99a
DCrepper <zoli.szabok@gmail.com>
2024-01-27 21:25:35 +0100

    update to 32

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,8 +22,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(function (User $user, string $ability) {
-            return $user->isSuperAdmin() ? true : null;
-        });
+        
     }
 }
