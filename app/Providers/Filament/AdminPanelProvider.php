fcb0b126d862dadb3fef0fc8938ea3027d3cdd50
Szabó Zoltán <zoli.szabok@gmail.com>
2024-05-24 10:59:25 +0200

    chore: Update composer.json and composer.lock with filament/notifications package

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->passwordReset()
             ->emailVerification()
             ->authGuard('web')
+            ->databaseNotifications()
             ->colors([
                 'primary' => Color::Amber,
             ])
